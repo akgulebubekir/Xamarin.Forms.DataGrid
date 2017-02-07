b9132750c94a4cc87ec547d837ed8b2741bf46b6
akgulebubekir <ebubekirakgul91@gmail.com>
2017-02-07 13:32:49 +0300

    Possible Sorting crash discovered & fixed

--- Xamarin.Forms.DataGrid/DataGrid.xaml.cs
+++ Xamarin.Forms.DataGrid/DataGrid.xaml.cs
@@ -415,7 +415,7 @@ namespace Xamarin.Forms.DataGrid
 		#region Sorting methods
 		private void SortItems(int propertyIndex, bool changeOrder = true)
 		{
-			if (ItemsSource == null || !Columns[propertyIndex].SortingEnabled)
+			if (ItemsSource == null || Columns.Count < propertyIndex && !Columns[propertyIndex].SortingEnabled)
 				return;
 
 			if (!IsSortable)
