3b49363d38b20eae824a75e3f10859bf5135a1b0
akgulebubekir <ebubekirakgul91@gmail.com>
2018-05-05 14:19:25 +0300

    Test VM and Models added

---
+++ Xamarin.Forms.DataGrid.UnitTest/Models/Team.cs
@@ -0,0 +1,17 @@
+﻿namespace Xamarin.Forms.DataGrid.UnitTest.Models
+{
+	public class Team
+	{
+		public string Name { get; set; }
+		public int Win { get; set; }
+		public int Loose { get; set; }
+		public double Percentage { get; set; }
+		public string Conf { get; set; }
+		public string Div { get; set; }
+		public string Home { get; set; }
+		public string Road { get; set; }
+		public string Last10 { get; set; }
+		public string Streak { get; set; }
+		public string Logo { get; set; }
+	}
+}
---
+++ Xamarin.Forms.DataGrid.UnitTest/VM/SingleVM.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+
+namespace Xamarin.Forms.DataGrid.UnitTest.VM
+{
+	internal class SingleVM<T> : INotifyPropertyChanged
+	{
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		private T _item;
+		public T Item
+		{
+			get { return _item; }
+			set
+			{
+				_item = value;
+				PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Item)));
+			}
+		}
+	}
+}
