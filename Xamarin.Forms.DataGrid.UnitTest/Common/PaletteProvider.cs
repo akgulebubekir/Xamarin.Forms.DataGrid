dd47b9742c31c2f0ad39ef042206ba9ad9c2511b
akgulebubekir <ebubekirakgul91@gmail.com>
2018-05-07 11:12:58 +0300

    Palette Provider for testing & unused usings removed

---
+++ Xamarin.Forms.DataGrid.UnitTest/Common/PaletteProvider.cs
@@ -0,0 +1,13 @@
+﻿namespace Xamarin.Forms.DataGrid.UnitTest.Common
+{
+	delegate Color GetColorDelegate(int index, object item);
+	public class PaletteProvider : IColorProvider
+	{
+		internal event GetColorDelegate OnColorRequested;
+
+		public Color GetColor(int rowIndex, object item)
+		{
+			return OnColorRequested(rowIndex, item);
+		}
+	}
+}
