4e81ef3030f49d4ef7227c7d9847910c6223ab8d
akgulebubekir <ebubekirakgul91@gmail.com>
2018-05-07 13:24:30 +0300

    NoDataView tests added

---
+++ Xamarin.Forms.DataGrid.UnitTest/NoDataViewTest.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Xamarin.Forms.DataGrid.UnitTest
+{
+	[TestClass]
+	public class NoDataViewTest
+	{
+		[ClassInitialize]
+		public static void Init(TestContext context)
+		{
+			MockPlatform.MockForms.Init();
+		}
+
+		#region Tests
+		[TestMethod]
+		public void NoDataViewInit()
+		{
+			var dg = new DataGrid();
+
+			var label = new Label {
+				Text = "noData"
+			};
+
+			Assert.IsTrue(dg.NoDataView == null);
+
+			dg.NoDataView = label;
+			Assert.IsTrue(dg.NoDataView == label);
+		}
+		#endregion
+
+	}
+}
