f70b58adf954b904c3c2282af7d92e1d5f9f94ca
akgulebubekir <ebubekirakgul91@gmail.com>
2022-10-17 19:20:36 +0200

    Upgrade Xamarin.Forms version to 5.0.0.2515

--- DataGridSample/DataGridSample.Android/Resources/Resource.designer.cs
+++ DataGridSample/DataGridSample.Android/Resources/Resource.designer.cs
@@ -14,7 +14,7 @@ namespace DataGridSample.Droid
 {
 	
 	
-	[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Android.Build.Tasks", "1.0.0.0")]
+	[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Android.Build.Tasks", "13.0.0.73")]
 	public partial class Resource
 	{
 		
