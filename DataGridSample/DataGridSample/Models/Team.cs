08e2b5b3d5a08413c0056b30d1a5c00c7c6f793d
akgulebubekir <ebubekirakgul91@gmail.com>
2023-01-30 19:14:11 +0100

    Update example with custom streak object and custom sorting logic

--- DataGridSample/DataGridSample/Models/Team.cs
+++ DataGridSample/DataGridSample/Models/Team.cs
@@ -1,17 +1,48 @@
-﻿namespace DataGridSample.Models
+﻿using System;
+
+namespace DataGridSample.Models
 {
-	public class Team
-	{
-		public string Name { get; set; }
-		public int Win { get; set; }
-		public int Loose { get; set; }
-		public double Percentage { get; set; }
-		public string Conf { get; set; }
-		public string Div { get; set; }
-		public string Home { get; set; }
-		public string Road { get; set; }
-		public string Last10 { get; set; }
-		public string Streak { get; set; }
-		public string Logo { get; set; }
-	}
+  public class Team
+  {
+    public string Name { get; set; }
+    public int Win { get; set; }
+    public int Loose { get; set; }
+    public double Percentage { get; set; }
+    public string Conf { get; set; }
+    public string Div { get; set; }
+    public string Home { get; set; }
+    public string Road { get; set; }
+    public string Last10 { get; set; }
+    public Streak Streak { get; set; }
+    public string Logo { get; set; }
+  }
+
+  public class Streak : IComparable
+  {
+    public Result Result { get; set; }
+    public int NumStreak { get; set; }
+
+    public int CompareTo(object other)
+    {
+      var r = Result == Result.Win ? NumStreak : -NumStreak;
+      if (other is Streak s)
+      {
+        var or =  s.Result == Result.Win? s.NumStreak : -s.NumStreak;
+        return r - or;
+      }
+
+      return r;
+    }
+
+    public override string ToString()
+    {
+      return $"{Enum.GetName(typeof(Result), Result)} {NumStreak}";
+    }
+  }
+
+  public enum Result
+  {
+    Loose = 0,
+    Win
+  }
 }
--- DataGridSample/DataGridSample/Views/Converters/StreakToColorConverter.cs
+++ DataGridSample/DataGridSample/Views/Converters/StreakToColorConverter.cs
@@ -1,39 +1,46 @@
 ﻿using System;
 using System.Globalization;
-using System.Linq;
-
+using DataGridSample.Models;
 using Xamarin.Forms;
 
 namespace DataGridSample.Views.Converters
 {
-	public class StreakToColorConverter : IValueConverter
-	{
-		public static string[] WinStreakColors = new string[] { "#CEF6CE", "#A9F5A9", "#81F781", "#58FA58", "#2EFE2E", "#00FF00", "#01DF01" };
-		public static string[] LooseStreakColors = new string[] { "#F5A9A9", "#F78181", "#FA5858", "#FE2E2E", "#FF0000", "#DF0101", "8A0808" };
+  public class StreakToColorConverter : IValueConverter
+  {
+    public static string[] WinStreakColors = new[]
+      { "#CEF6CE", "#A9F5A9", "#81F781", "#58FA58", "#2EFE2E", "#00FF00", "#01DF01" };
 
-		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-		{
-			if (value == null)
-				return Color.Transparent;
+    public static string[] LooseStreakColors = new[]
+      { "#F5A9A9", "#F78181", "#FA5858", "#FE2E2E", "#FF0000", "#DF0101", "8A0808" };
 
-			var val = value.ToString();
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+      if (value == null)
+      {
+        return Color.Transparent;
+      }
 
-			var tokens = val.Split(' ');
-			if (tokens.Length != 2)
-				throw new ArgumentException("incorrect streak format");
+      else
+      {
+        if (value is Streak s)
+        {
+          if (s.Result == Result.Win)
+            return Color.FromHex(WinStreakColors.Length > s.NumStreak
+              ? WinStreakColors[s.NumStreak]
+              : WinStreakColors[WinStreakColors.Length - 1]);
 
-			if (!int.TryParse(tokens[1], out var numStreak))
-				throw new ArgumentException("incorrect streak format");
+          return Color.FromHex(LooseStreakColors.Length > s.NumStreak
+            ? LooseStreakColors[s.NumStreak]
+            : LooseStreakColors[LooseStreakColors.Length - 1]);
+        }
 
-			if (tokens.First() == "W")
-				return Color.FromHex((WinStreakColors.Length > numStreak) ? WinStreakColors[numStreak] : WinStreakColors[WinStreakColors.Length - 1]);
-			else
-				return Color.FromHex((LooseStreakColors.Length > numStreak) ? LooseStreakColors[numStreak] : LooseStreakColors[LooseStreakColors.Length - 1]);
-		}
+        return Color.Orange;
+      }
+    }
 
-		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-		{
-			throw new NotImplementedException();
-		}
-	}
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+      throw new NotImplementedException();
+    }
+  }
 }
